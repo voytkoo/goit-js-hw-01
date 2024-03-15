89c8b661713c5d59fc9e728a93c666e344c781f5
voytkoo <avoytko86@gmail.com>
2024-03-15 17:39:52 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,8 @@ function getElementWidth(content, padding, border) {
     content = parseFloat(content);
     padding = parseFloat(padding);
     border = parseFloat(border);
-    return content + padding * 2 + border * 2;
+    const borderBox = content + padding * 2 + border * 2;
+    return borderBox;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
