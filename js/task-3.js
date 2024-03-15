f13578ad35de4b7f24be2deb8e1148683e7547af
voytkoo <avoytko86@gmail.com>
2024-03-15 11:02:49 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,10 @@
 function getElementWidth(content, padding, border) {
-    const Npx = 
+    content = parseInt(content);
+    padding = parseInt(padding);
+    border = parseInt(border);
+    return content + padding * 2 + border * 2;
 }
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+
